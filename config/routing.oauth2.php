90b51746df81be47a31bf7a3adad223e8c89593e
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2017-06-21 13:30:39 -0400

    Update OAuth urls

--- config/routing.oauth2.php
+++ config/routing.oauth2.php
@@ -5,13 +5,13 @@
 
 // [POST] Generate token
 if(class_exists('App\Controllers\OAuth2\OAuth2GenerateAccessTokenController')) {
-	$app->post('/auth/generate-token', [new App\Controllers\OAuth2\OAuth2GenerateAccessTokenController(), 'generateToken']);
+	$app->post('/oauth/access_token', [new App\Controllers\OAuth2\OAuth2GenerateAccessTokenController(), 'generateToken']);
 }
 
 // [GET|POST|PUT|DELETE] Test
 if(class_exists('App\Controllers\OAuth2\OAuth2TestController')) {
-	$app->get('/auth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
-	$app->post('/auth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
-	$app->put('/auth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
-	$app->delete('/auth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
+	$app->get('/oauth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
+	$app->post('/oauth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
+	$app->put('/oauth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
+	$app->delete('/oauth/test', [new App\Controllers\OAuth2\OAuth2TestController(), 'test']);
 }
